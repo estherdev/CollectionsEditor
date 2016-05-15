722738e16a000d7a740b8df6c0c94b0147f2c04b
estherdev <estherwork@Esthers-MBP.home>
2016-05-15 02:55:21 +0100

    Created a child component called CollectionGroupList containing placeholder text

--- public/scripts/collections.js
+++ public/scripts/collections.js
@@ -2,11 +2,25 @@ var CollectionBox = React.createClass({
   render: function() {
     return (
       <div>
-        
+        <CollectionGroupList />
       </div>
     );
   }
 });
+
+var CollectionGroupList = React.createClass({
+  render: function() {
+      return (
+       <div>
+        <h1>I am a list of Collection Groups</h1>
+       </div>
+      );
+ 
+}
+});
+
+
+
 
 ReactDOM.render(
   <CollectionBox />,
