d1e59cd23e4a6031dad4c80602c554062cda9277
estherdev <estherwork@Esthers-MBP.home>
2016-05-15 05:03:36 +0100

    Added card styling to the CollectionGroup component

--- public/scripts/collections.js
+++ public/scripts/collections.js
@@ -25,14 +25,45 @@ var CollectionGroup = React.createClass({
   render: function() {
     return (
       <div>
-        <h2>Collection Group</h2>
-        <div className="collectionGroup">{this.props.name}</div>
-        <div className="slug">{this.props.slug}</div>
-        <div className="description">{this.props.description}</div>
+        
+        <div className="mdl-layout mdl-js-layout">
+	<main className="mdl-layout__content">
+		<div className="mdl-card mdl-shadow--6dp">
+			<div className="mdl-card__title mdl-color--primary mdl-color-text--white">
+				<h2 className="mdl-card__title-text">{this.props.name}</h2>
+			</div>
+	  	<div className="mdl-card__supporting-text">
+				<form action="#">
+					<div className="mdl-textfield mdl-js-textfield">
+          <b>Slug:</b>
+						<input className="mdl-textfield__input" type="text" id="slug" defaultValue={this.props.slug} />
+						<label className="mdl-textfield__label" for="slug"></label>
+					</div>
+					<div className="mdl-textfield mdl-js-textfield">
+          <b>Description:</b>
+						<input className="mdl-textfield__input" type="text" id="description" defaultValue={this.props.description} />
+						<label className="mdl-textfield__label" for="description"></label>
+					</div>          
+				</form>
+			</div>
+			<div className="mdl-card__actions mdl-card--border">
+				<button className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Edit</button>
+			</div>
+		</div>
+	</main>
+</div>
+        
       </div>
+      
+      
+      
+      
+      
+      
     );  
   }
 });
+
 
 
 ReactDOM.render(
