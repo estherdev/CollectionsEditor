d7177ed7ecb91f63d6430b4d694ef2bf88a1d8f2
estherdev <estherwork@Esthers-MBP.home>
2016-05-15 14:51:15 +0100

    Made modifications to dynamically render the collectionGroup data and added a card for Collection details

--- public/scripts/collections.js
+++ public/scripts/collections.js
@@ -2,7 +2,36 @@ var CollectionBox = React.createClass({
   render: function() {
     return (
       <div>
+       <div>
+      <div className="mdl-layout mdl-js-layout">
+	    <main className="mdl-layout__content">
+		  <div className="mdl-card mdl-shadow--6dp">
+			<div className="mdl-card__title mdl-color--primary mdl-color-text--white">
+				<h2 className="mdl-card__title-text">Subject Collection</h2>
+			</div>
+	  	<div className="mdl-card__supporting-text">
+			<form action="#">
+        <div className="mdl-textfield mdl-js-textfield">
+          <b>Slug:</b>
+          <input className="mdl-textfield__input" type="text" id="slug" defaultValue="subject-collection" />
+          <label className="mdl-textfield__label" for="slug"></label>
+        </div>
+        <div className="mdl-textfield mdl-js-textfield">
+            <b>Description:</b>
+            <input className="mdl-textfield__input" type="text" id="description" defaultValue="This is a collection of subjects" />
+            <label className="mdl-textfield__label" for="description"></label>
+        </div>          
+			</form>
+			</div>
+			<div className="mdl-card__actions mdl-card--border">
+				<button className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Edit</button>
+			</div>
+		</div>
+	</main>
+</div>
+              <h3>The Collection Groups:</h3>
         <CollectionGroupList data={this.props.data} />
+      </div>
       </div>
     );
   }
@@ -10,28 +39,28 @@ var CollectionBox = React.createClass({
 
 var CollectionGroupList = React.createClass({
   render: function() {
+    var collectionGroupNodes = this.props.data.map(function(collectionGroup) {
       return (
        <div>
-       
-       <div className="mdl-layout mdl-js-layout">
-	<main className="mdl-layout__content">
-		<div className="mdl-card mdl-shadow--6dp">
+      <div className="mdl-layout mdl-js-layout">
+	    <main className="mdl-layout__content">
+		  <div className="mdl-card mdl-shadow--6dp">
 			<div className="mdl-card__title mdl-color--primary mdl-color-text--white">
-				<h2 className="mdl-card__title-text">Subject Collection</h2>
+				<h2 className="mdl-card__title-text">{collectionGroup.name}</h2>
 			</div>
 	  	<div className="mdl-card__supporting-text">
-				<form action="#">
-					<div className="mdl-textfield mdl-js-textfield">
+			<form action="#">
+        <div className="mdl-textfield mdl-js-textfield">
           <b>Slug:</b>
-						<input className="mdl-textfield__input" type="text" id="slug" defaultValue="subject-collection" />
-						<label className="mdl-textfield__label" for="slug"></label>
-					</div>
-					<div className="mdl-textfield mdl-js-textfield">
-          <b>Description:</b>
-						<input className="mdl-textfield__input" type="text" id="description" defaultValue="Here are the subjects" />
-						<label className="mdl-textfield__label" for="description"></label>
-					</div>          
-				</form>
+          <input className="mdl-textfield__input" type="text" id="slug" defaultValue={collectionGroup.slug} />
+          <label className="mdl-textfield__label" for="slug"></label>
+        </div>
+        <div className="mdl-textfield mdl-js-textfield">
+            <b>Description:</b>
+            <input className="mdl-textfield__input" type="text" id="description" defaultValue={collectionGroup.description} />
+            <label className="mdl-textfield__label" for="description"></label>
+        </div>          
+			</form>
 			</div>
 			<div className="mdl-card__actions mdl-card--border">
 				<button className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Edit</button>
@@ -40,10 +69,15 @@ var CollectionGroupList = React.createClass({
 	</main>
 </div>
 
-        <h3>The Collection Groups:</h3>
-        <CollectionGroup name="Maths" slug="maths-topics" description="About Maths"></CollectionGroup>
-        <CollectionGroup name="Science" slug="science-topics" description="About Maths"></CollectionGroup>
+
        </div>
+
+    );
+          })
+    return (
+      <div className="collectionGroupList">
+        {collectionGroupNodes}
+       </div> 
       );
  
 }
@@ -53,35 +87,10 @@ var CollectionGroup = React.createClass({
   render: function() {
     return (
       <div>
-        
-        <div className="mdl-layout mdl-js-layout">
-	<main className="mdl-layout__content">
-		<div className="mdl-card mdl-shadow--6dp">
-			<div className="mdl-card__title mdl-color--primary mdl-color-text--white">
-				<h2 className="mdl-card__title-text">{this.props.name}</h2>
-			</div>
-	  	<div className="mdl-card__supporting-text">
-				<form action="#">
-					<div className="mdl-textfield mdl-js-textfield">
-          <b>Slug:</b>
-						<input className="mdl-textfield__input" type="text" id="slug" defaultValue={this.props.slug} />
-						<label className="mdl-textfield__label" for="slug"></label>
-					</div>
-					<div className="mdl-textfield mdl-js-textfield">
-          <b>Description:</b>
-						<input className="mdl-textfield__input" type="text" id="description" defaultValue={this.props.description} />
-						<label className="mdl-textfield__label" for="description"></label>
-					</div>          
-				</form>
-			</div>
-			<div className="mdl-card__actions mdl-card--border">
-				<button className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Edit</button>
-			</div>
-		</div>
-	</main>
-</div>
-        
-      </div>      
+      <div className="collectionGroup">{this.props.name}</div>
+      <div className="slug">{this.props.slug}</div>
+      <div className="description">{this.props.description}</div>
+      </div>
     );  
   }
 });
