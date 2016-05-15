92d86d474a8dc64cfe50611a203f880dc00cde0a
estherdev <estherwork@Esthers-MBP.home>
2016-05-15 05:36:15 +0100

    Added a card for the Collections to the CollectionGroupList component

--- public/scripts/collections.js
+++ public/scripts/collections.js
@@ -12,7 +12,35 @@ var CollectionGroupList = React.createClass({
   render: function() {
       return (
        <div>
-        <h1>I am a list of Collection Groups</h1>
+       
+       <div className="mdl-layout mdl-js-layout">
+	<main className="mdl-layout__content">
+		<div className="mdl-card mdl-shadow--6dp">
+			<div className="mdl-card__title mdl-color--primary mdl-color-text--white">
+				<h2 className="mdl-card__title-text">Subject Collection</h2>
+			</div>
+	  	<div className="mdl-card__supporting-text">
+				<form action="#">
+					<div className="mdl-textfield mdl-js-textfield">
+          <b>Slug:</b>
+						<input className="mdl-textfield__input" type="text" id="slug" defaultValue="subject-collection" />
+						<label className="mdl-textfield__label" for="slug"></label>
+					</div>
+					<div className="mdl-textfield mdl-js-textfield">
+          <b>Description:</b>
+						<input className="mdl-textfield__input" type="text" id="description" defaultValue="Here are the subjects" />
+						<label className="mdl-textfield__label" for="description"></label>
+					</div>          
+				</form>
+			</div>
+			<div className="mdl-card__actions mdl-card--border">
+				<button className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Edit</button>
+			</div>
+		</div>
+	</main>
+</div>
+
+        <h3>The Collection Groups:</h3>
         <CollectionGroup name="Maths" slug="maths-topics" description="About Maths"></CollectionGroup>
         <CollectionGroup name="Science" slug="science-topics" description="About Maths"></CollectionGroup>
        </div>
